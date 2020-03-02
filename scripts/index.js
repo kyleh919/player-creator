a7ea07a23e2fb8241640ef289e120f5dee07a9b7
Kyle Hunter <kyle.hunter91@gmail.com>
2020-03-02 00:23:59 -0800

    Adds Player class

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
-console.log('it works');
+class Player {
+  constructor() {
+    this.firstName_ = '';
+    this.lastName_ = '';
+  }
+
+  setFirstName(firstName) {
+    this.firstName_ = firstName;
+  }
+}
+
+const newPlayer = new Player();
