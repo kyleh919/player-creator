37b9e895034dbe891e6163be6dfde4e89e243ce9
Kyle Hunter <kyle.hunter91@gmail.com>
2020-03-02 00:26:08 -0800

    Includes getters/setters for first and last name

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,20 @@ class Player {
     this.lastName_ = '';
   }
 
+  getFirstName() {
+    return this.firstName_;
+  }
+
   setFirstName(firstName) {
     this.firstName_ = firstName;
+  }
+
+  getLastName() {
+    return this.lastName_;
+  }
+
+  setLastName(lastName) {
+    this.lastName_ = lastName;
   }
 }
 
